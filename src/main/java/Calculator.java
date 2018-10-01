e60b3bc7669e1303cb35d20bc3a55139e1e51831
celfons <marcel.resende@yahoo.com.br>
2018-09-30 23:07:12 -0300

    refactoring test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,19 +1,48 @@
 public class Calculator {
 
-    public int sum(int number1, int number2){
+    private int number1;
+    private int number2;
+
+    public Calculator(int number1, int number2) {
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+
+    public int getNumber1() {
+        return number1;
+    }
+
+    public void setNumber1(int number1) {
+        this.number1 = number1;
+    }
+
+    public int getNumber2() {
+        return number2;
+    }
+
+    public void setNumber2(int number2) {
+        this.number2 = number2;
+    }
+
+    public int sum(){
 
         return number1 + number2;
 
     }
 
-    public int multiplication(int number1, int number2){
+    public int multiplication(){
 
         return number1 * number2;
     }
 
-    public double multiplication(int number1, int number2, int number3){
+    public double multiplication(int number3){
 
         return number1 * number2 * number3;
+    }
+
+    public int diff(){
+
+        return number1-number2;
     }
 
     public int diff(int number1, int number2){
--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,7 +1,11 @@
 public class Triangle extends Calculator {
 
+    public Triangle(int number1, int number2) {
+        super(number1, number2);
+    }
+
     @Override
-    public int multiplication(int number1, int number2){
-        return (number1 * number2) / 2 ;
+    public int multiplication(){
+        return (this.getNumber1() * this.getNumber2()) / 2 ;
     }
 }
