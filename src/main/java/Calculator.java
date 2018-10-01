a4bb298f7d64b4ae2989500452b6c2ccf4fd844b
guilh <carvalhogsc@gmail.com>
2018-09-30 23:05:56 -0300

    test ok, sobrecarga

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,25 @@
 public class Calculator {
 
-    public int sum(int number1, int number2){
+    public int number1;
+    public int number2;
+
+    public Calculator(int number1, int number2){
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+
+    public int sum(){
 
         return number1 + number2;
 
     }
 
-    public int multiplication(int number1, int number2){
+    public int multiplication(){
 
         return number1 * number2;
     }
 
-    public double multiplication(int number1, int number2, int number3){
+    public double multiplication(int number3){
 
         return number1 * number2 * number3;
     }
--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,7 +1,13 @@
 public class Triangle extends Calculator {
 
+    public Triangle(int number1, int number2)
+    {
+        super(number1, number2);
+
+    }
+
     @Override
-    public int multiplication(int number1, int number2){
+    public int multiplication(){
         return (number1 * number2) / 2 ;
     }
 }
